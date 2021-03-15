68693271dc6efa773d7b2830282ebeea2fe92e50
Kalin Jack <kalinj1997@gmail.com>
2021-03-15 12:28:00 -0400

    Done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 31 
+const num2 = 2
+let multiply = num1 * num2;
+// return multiply;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 16 % 6;
+
+const num3= 4
+
+const num4= 6
+
+const max = Math.max(16, 17, 18, 19, 20);
